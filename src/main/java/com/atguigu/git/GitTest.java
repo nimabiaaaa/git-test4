b8c9fd442bc3117c62edf0931bf3d5307b2b405d
Liao <164808354@qq.com>
2022-04-09 17:35:09 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author: Mr_liao
+ * @create: 2022-04-09 17:16
+ * @description:
+ **/
+public class GitTest {
+    public static void main(String[] args){
+        System.out.println("hello git!");
+    }
+}
